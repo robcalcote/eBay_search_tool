4388334edfee330b61765c6d84663e1135918eaa
Rob <robcalcote@gmail.com>
2019-10-11 11:02:13 -0600

    created local database and connected - hid info file

--- js_ebay_search/settings.py
+++ js_ebay_search/settings.py
@@ -74,11 +74,9 @@ WSGI_APPLICATION = 'js_ebay_search.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dev_settings.dev_Database
 }
+
 
 
 # Password validation
